55e7dfe50d1af6d0d3f4603a0e4d2f6f88a1e232
Wilson Huang <wilsonhuang11012000@gmail.com>
2024-07-19 14:25:35 -0400

    change cors allow

--- comments/settings.py
+++ comments/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://commentinterviewprep.s3-website.us-east-2.amazonaws.com"
